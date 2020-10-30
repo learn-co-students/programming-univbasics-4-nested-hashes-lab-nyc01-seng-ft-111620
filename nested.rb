c9a8f3f4e8a6272e5281ebbfaaea4d9780d491b5
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-30 20:21:28 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,10 +52,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,6 +79,10 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+      },
+      :yukihiro_matsumoto => {
+        known_for: "Ruby",
+        languages: ["LISP", "C"]
       }
     }
 
@@ -104,6 +108,9 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = "GUI"
+
+programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +133,7 @@ def adding_to_dennis
       }
     }
 
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+
+programmer_hash
 end
